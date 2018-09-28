7e8ca4d8930585df5fa170ded10897e79da05bd3
DrCoffey <39605011+DrCoffey@users.noreply.github.com>
2018-09-28 10:46:58 -0700

    V1.1.2 Users can set time display range of spectrogram

--- Functions/update_fig.m
+++ Functions/update_fig.m
@@ -17,7 +17,13 @@ end
 set(handles.axes1,'YDir', 'normal','YColor',[1 1 1],'XColor',[1 1 1],'Clim',[0 2*mean(max(I))]);
 % colormap(handles.axes1,handles.cmap);
 set(handles.spect,'CData',imgaussfilt(abs(s)),'XData',ti,'YData',fr/1000);
-set(handles.axes1,'Xlim',[handles.spect.XData(1) handles.spect.XData(end)])
+
+if handles.settings.DisplayTimePadding
+    meantime = handles.calls(handles.currentcall).RelBox(1) + handles.calls(handles.currentcall).RelBox(3)/2;
+    set(handles.axes1,'Xlim',[meantime-.1,meantime+.1],'color','k')
+else
+    set(handles.axes1,'Xlim',[handles.spect.XData(1) handles.spect.XData(end)])
+end
 
 set(handles.axes1,'ylim',[handles.settings.LowFreq handles.settings.HighFreq]);
 % xlim([min(ti) max(ti)]);
