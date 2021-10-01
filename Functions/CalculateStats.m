6c198786d1eec0770657a76cdec30ff1b8db78a8
Jay Bladon <bladonjay@gmail.com>
2021-09-30 20:42:24 -0400

    Revert "Update CalculateStats.m"
    This reverts commit 011b8b83a95dba61bdd29e87d74f32bb51b2a8e3.

--- Functions/CalculateStats.m
+++ Functions/CalculateStats.m
@@ -35,8 +35,8 @@ stats.Entropy = smooth(stats.Entropy,0.1,'rlowess')';
 if AmplitudeThreshold > .001 & AmplitudeThreshold < .999
     brightThreshold=prctile(I(:),AmplitudeThreshold*100);
 else
-    %disp('Warning! Amplitude Percentile Threshold Must be (0 > 1), Reverting to Default (.825)');
-    brightThreshold=prctile(I(:),.825);
+    disp('Warning! Amplitude Percentile Threshold Must be (0 > 1), Reverting to Default (.825)');
+    brightThreshold=prctile(I(:),825);
 end
 
 if EntropyThreshold < .001 | EntropyThreshold > .999 
